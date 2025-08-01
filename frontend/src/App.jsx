124b99a5e854f065255a0b93cd69a953db3f069c
iamsayannath <sayannath441@gmail.com>
2025-08-01 22:00:37 +0530

    home remove

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import Givefeed from './pages/Givefeed';
 import Getallrecipe from './pages/Getallrecipe';
 import Contactus from './pages/Contactus';
-import Home from './pages/Home';
 import Notfound from './pages/Notfound';
 import Recipedetails from './pages/Recipedetails';
 import { Routes, Route } from 'react-router-dom';
@@ -18,7 +17,7 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Getallrecipe />} />
         <Route path="/givefeed" element={<Givefeed />} />
         <Route path="/getallrecipe" element={<Getallrecipe />} />
         <Route path="/contactus" element={<Contactus />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,12 +5,11 @@ const Navbar = () => {
   return (
     <nav className="h-[70px] relative w-full px-6 md:px-16 lg:px-24 xl:px-32 flex items-center justify-between z-20 bg-white text-gray-700 shadow-[0px_4px_25px_0px_#0000000D] transition-all">
 
-      <Link to="/" className="text-green-600 font-bold text-2xl">
+      <Link to="/getallrecipe" className="text-green-600 font-bold text-2xl">
         <h1>Recipe Finder</h1>
       </Link>
 
       <ul className="md:flex hidden items-center gap-10">
-        <li><Link className="hover:text-gray-500/80 transition" to="/">Home</Link></li>
         <li><Link className="hover:text-gray-500/80 transition" to="/getallrecipe">Get all recipe</Link></li>
         <li><Link className="hover:text-gray-500/80 transition" to="/contactus">Contact Us</Link></li>
       </ul>
